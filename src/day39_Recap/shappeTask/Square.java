b4e7d0a6d3b57550a406156a4c070838095688bc
Jasmine <bayramali5219@gmail.com>
2022-04-13 16:02:56 +0300

    Shape Task completed

--- src/day39_Recap/shappeTask/Square.java
+++ src/day39_Recap/shappeTask/Square.java
@@ -1,10 +1,40 @@
 package day39_Recap.shappeTask;
 
 public class Square extends Shape {
+    private double side;
 
-    public Square(String name) {
-        super(name);
+    private double getSide(){
+        return side;
+    }
+    public void setSide(double side){
+        if(side<=0){
+            System.err.println("Invalid side : "+side);
+            System.exit(1);
+        }
+        this.side=side;
     }
 
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
 
+    @Override
+    public double area() {
+        return side*side;
+    }
+
+    @Override
+    public double perimeter() {
+        return 4*side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area =" + area() +
+                ", perimeter" + perimeter() +
+                '}';
+    }
 }
