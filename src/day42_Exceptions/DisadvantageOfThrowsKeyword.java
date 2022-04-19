74f2c78f2f1a88f63b836b53e7b09c092eb02388
Jasmine <bayramali5219@gmail.com>
2022-04-19 12:49:23 +0300

    Throws keyword vs try-catch

---
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword.java
@@ -0,0 +1,22 @@
+package day42_Exceptions;
+
+public class DisadvantageOfThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello");
+        sleep(2.5);
+        System.out.println("Hello World");
+        System.out.println("-----------------------------------");
+        System.out.println("Hello");
+        MorningWorkOut2.sleep(2.5); // try & catch
+        System.out.println("Hello World");
+        System.out.println("-----------------------------------");
+
+
+    }
+
+    public static void sleep(double seconds) throws InterruptedException {
+
+            Thread.sleep((long)(seconds*1000));
+
+    }
+}
