910e16ad020dbd002d91651a36cf3e3ca48fe0da
Jasmine <bayramali5219@gmail.com>
2022-04-19 11:48:40 +0300

    timer

---
+++ src/day42_Exceptions/MorninigWorkout.java
@@ -0,0 +1,36 @@
+package day42_Exceptions;
+
+public class MorninigWorkout {
+    public static void main(String[] args) {
+        System.out.println(" -----------------------Push up started-----------------");
+
+        for (int i = 0; i <=30; i++) {
+            System.out.print("\rPush up "+ i);
+
+            try{
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n-----------------------Push up completed-----------------");
+
+        System.out.println(" -----------------------Pull up started-----------------");
+
+        for (int i = 0; i <=20; i++) {
+            System.out.print("\rPull up "+ i);
+
+            try{
+                Thread.sleep(2500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n-----------------------Pull up completed-----------------");
+
+
+    }
+
+
+
+}
