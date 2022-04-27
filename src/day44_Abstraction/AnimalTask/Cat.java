c4db8cd89ac1045f1fb2afac5b4e929db5c705fd
Jasmine <bayramali5219@gmail.com>
2022-04-27 04:49:02 +0300

    Abstract class and abstract object

---
+++ src/day44_Abstraction/AnimalTask/Cat.java
@@ -0,0 +1,11 @@
+package day44_Abstraction.AnimalTask;
+
+public class Cat extends Animal{
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    public void eat(){
+        System.out.println(getName()+" eats biryani");
+    }
+}
