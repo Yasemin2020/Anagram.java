d29a4d7c779b68f1f179625f616a6f672b4040f6
Jasmine <bayramali5219@gmail.com>
2022-05-04 18:45:47 +0300

    day45 interface

--- src/day44_Abstraction/AnimalTask/Cat.java
+++ src/day44_Abstraction/AnimalTask/Cat.java
@@ -1,11 +1,16 @@
 package day44_Abstraction.AnimalTask;
 
-public class Cat extends Animal{
+public class Cat extends Animal implements Playable{
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
 
     public void eat(){
         System.out.println(getName()+" eats biryani");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(getName()+" is playing");
     }
 }
--- src/day44_Abstraction/AnimalTask/Eagle.java
+++ src/day44_Abstraction/AnimalTask/Eagle.java
@@ -1,10 +1,13 @@
 package day44_Abstraction.AnimalTask;
 
-public class Eagle extends Animal{
+public class Eagle extends Animal implements WildAnimal{
     public Eagle(String name, String breed, char gender, int age, String size, String color){
         super(name, breed, gender, age , size, color);
     }
     public void eat(){
         System.out.println(getName()+ " eats snake");
+    }
+    public void hunt(){
+        System.out.println(getName()+" hunts snake");
     }
 }
