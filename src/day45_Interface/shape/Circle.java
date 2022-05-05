33e5754a3889d57b29adb336aac32944988d12e9
Jasmine <bayramali5219@gmail.com>
2022-05-05 11:07:23 +0300

    day45 Abstract Interface Shape Task

--- src/day45_Interface/shape/Circle.java
+++ src/day45_Interface/shape/Circle.java
@@ -1,4 +1,17 @@
 package day45_Interface.shape;
 
 public class Circle {
+    private double radius;
+    public final static double pi=3.14;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius<0){
+            throw new RuntimeException("Invalid radius: "+radius);
+        }
+        this.radius = radius;
+    }
 }
