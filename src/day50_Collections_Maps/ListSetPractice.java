f75cb783dc3e2982c232f7f719e01d154610a972
Jasmine <bayramali5219@gmail.com>
2022-05-20 11:12:38 +0300

    Collections Queue

--- src/day50_Collections_Maps/ListSetPractice.java
+++ src/day50_Collections_Maps/ListSetPractice.java
@@ -1,6 +1,26 @@
 package day50_Collections_Maps;
 
-public class ListSetPractice {
+import java.util.*;
 
+public class ListSetPractice {
+    public static void main(String[] args) {
+        Set<Integer> set =new HashSet<>();
+        set.addAll(Arrays.asList(1,2,3,4,5,1,2,3,4,5));
+
+        System.out.println(set);
+
+        //Integer[] array=set.toArray(new Integer[0]);//turning to an array
+
+        List<Integer> list = new ArrayList<>(set);
+        System.out.println(list);
+
+        List<String> names =null;
+
+        //System.out.println(names.size());//NullPointerException
+
+
+
+
+    }
 
 }
