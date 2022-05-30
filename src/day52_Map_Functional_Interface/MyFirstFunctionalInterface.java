56703a8ba1e483365f3e89e980e524a710477077
Jasmine <bayramali5219@gmail.com>
2022-05-30 14:00:15 +0300

    Functional Interface

---
+++ src/day52_Map_Functional_Interface/MyFirstFunctionalInterface.java
@@ -0,0 +1,10 @@
+package day52_Map_Functional_Interface;
+
+
+@FunctionalInterface
+public interface MyFirstFunctionalInterface {
+    void apply(int n);
+
+
+
+}
---
+++ src/day52_Map_Functional_Interface/Test.java
@@ -0,0 +1,23 @@
+package day52_Map_Functional_Interface;
+
+public class Test {
+    public static void main(String[] args) {
+        // function1: create a function that can display a number is odd
+
+
+        // function2: create a function that can check if a person is eligible to buy alcohol
+
+
+
+        //function3: create a function that can display the cube of a number
+
+
+
+
+
+        //function4: create a function that can check if a number is evenly divisble by  3 & 5
+
+
+
+    }
+}
