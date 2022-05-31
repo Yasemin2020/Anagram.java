0ba46b361b60215e275b0170db7ba9c05fc92c0d
Jasmine <bayramali5219@gmail.com>
2022-05-31 14:01:56 +0300

    Build in functional Interface

---
+++ src/day53_FunctionalInterface/BuildInFunctionalInterface2.java
@@ -0,0 +1,32 @@
+package day53_FunctionalInterface;
+
+import java.util.function.BiPredicate;
+
+public class BuildInFunctionalInterface2 {
+    public static void main(String[] args) {
+        //Create a function that can check if the Integer is contained in an array of integer, return boolean
+        BiPredicate<Integer[],Integer> contains = (a,b)-> {
+            boolean result =false;
+            for (Integer each : a) {
+                if (each==b){
+                    result =true;
+                }
+            }
+            return result;
+        };
+
+        Integer[] arr= {1,2,3,4,5,6,7,8,9,10};
+        boolean r1= contains.test(arr,11);
+
+        System.out.println(r1);
+
+
+
+
+
+
+
+
+    }
+
+}
