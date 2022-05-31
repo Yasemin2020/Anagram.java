c86aebc2a726da47684eeee4a76c5d705f12868e
Jasmine <bayramali5219@gmail.com>
2022-05-31 13:50:58 +0300

    Build in functional Interface

--- src/day53_FunctionalInterface/BuildInFunctionalInterface1.java
+++ src/day53_FunctionalInterface/BuildInFunctionalInterface1.java
@@ -1,5 +1,10 @@
 package day53_FunctionalInterface;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class BuildInFunctionalInterface1 {
@@ -11,6 +16,100 @@ public class BuildInFunctionalInterface1 {
 
         boolean result1 = isPalindrome.test("Java");
         System.out.println("result1 = " + result1);
+
+        System.out.println("--------------------------------------");
+
+        Predicate<Integer> isEven = p -> p % 2 == 0;
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3));
+       // list.removeIf(p -> p % 2 == 0);
+        list.removeIf(isEven);
+
+        System.out.println(list);
+
+        System.out.println("------------------------------------------");
+
+        List<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Level", "Anna", "Java", "Python"));
+
+        names.removeIf(isPalindrome);
+
+        System.out.println(names);
+
+
+        System.out.println("----------------------------------------");
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println(s.charAt(i));
+            }
+        };
+
+        printEach.accept("Java");
+
+        System.out.println("---------------------------------------------");
+
+        //find odd numbers
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+      /*  for (Integer each : list2) {
+            if (each%2!=0){
+                System.out.println(each);
+            }
+        }*/
+        list2.forEach(p->{if(p%2!=0) System.out.println(p);});
+
+        System.out.println("----------------------------------------------------");
+        //Take the initials A.A
+
+        List<String> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList("Anel Adilbayeva", "Aygun Abbasova Jarullah",
+                "Jilili Jiwuer", "Ali Ismayilov"));
+        employees.forEach(s -> System.out.println(s.charAt(0)+"."+s.charAt(s.lastIndexOf(" ")+1)));
+
+        System.out.println("----------------------------------------------------");
+// coverts an Array to List
+        Function<int[],List<Integer>> convertToList = (a)->{
+            List<Integer> result = new ArrayList<>();
+            for (int each : a) {
+                result.add(each);
+            }
+            return result;
+        };
+
+        int[] arr={1,2,3,4,5,6,7,8,9,10};
+        List<Integer> l =convertToList.apply(arr);
+        System.out.println(l);
+
+        System.out.println("----------------------------------------------------");
+        // coverts an List to Array
+        Function<List<Integer>, int[]> convertToArray= a-> {
+            int[] result = new int[a.size()];
+            for (int i = 0; i < a.size(); i++) {
+                result[i] = a.get(i);
+            }
+
+            return result;
+        };
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        int[] result2 = convertToArray.apply(numbers);
+
+        System.out.println("--------------------------------------");
+        // create a function that can return the maximum number from an int array
+
+        // create a function that can swap the first and last elements of an array
+
+        // Create a function that can reverse an array and returns it
+
+        // create a function that can reverse a List
+
+
+
+
+
+
+
 
     }
 }
